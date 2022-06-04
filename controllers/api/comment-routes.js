7c994c69b292ff05654221383e9b8264ec46f830
Aubree Alexander <aubree.alexander@gmail.com>
2022-06-04 11:14:11 -0500

    add basic comment route

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,21 +1,16 @@
 //add in a basic comment route
 
 const router = require('express').Router();
-const Comment = require('../../models/Comment')
-// const withAuth = require('../../utils/auth')
+const Comment = require('../../models/Comment');
+const withAuth = require('../../utils/auth')
 
-//post comment
-//aa - either do an await or try here
-// router.post('/', withAuth, async (req, res) => {
-//     await Comment.create({ ...req.body }) //array of comments
-// })
 
-router.post('/', async (req, res) => {
+router.post('/', withAuth, async (req, res) => {
     try {
-        const postComment = await Comment.create({ ...req.body, userId: req.session.userId })
+        const postComment = await Comment.create({ ...req.body, userId: req.session.userId });
         res.json(postComment)
     } catch (err) {
-        res.status(400).json(err)
+        res.status(500).json(err)
     }
 });
 
